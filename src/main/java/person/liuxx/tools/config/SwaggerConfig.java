78c83f7488b08b2069006bdf3f903896e16021a6
dark-slayers <23534878@qq.com>
2018-03-30 09:15:04 +0800

    项目初始化

---
+++ src/main/java/person/liuxx/tools/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package person.liuxx.tools.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author 刘湘湘
+ * @version 1.0.0<br>
+ *          创建时间：2017年7月31日 下午3:02:34
+ * @since 1.0.0
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig
+{
+    @Bean
+    public Docket createRestApi()
+    {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("person.liuxx.tools.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo()
+    {
+        return new ApiInfoBuilder().title("tools Service APIs")
+                .description("tools Service ：http://localhost:8080/")
+                .termsOfServiceUrl("http://blog.didispace.com/")
+                .contact(new Contact("aa", "aa.aa.com", "aa@aa.com"))
+                .version("1.0")
+                .build();
+    }
+}
---
+++ src/main/java/person/liuxx/tools/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package person.liuxx.tools.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author 刘湘湘
+ * @version 1.0.0<br>
+ *          创建时间：2017年7月26日 下午4:38:54
+ * @since 1.0.0
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter
+{
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        // registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/dev/**").addResourceLocations(
+                "file:E:/GitProject/tools/dist/");
+        registry.addResourceHandler("/page/**").addResourceLocations("file:./page/");
+        super.addResourceHandlers(registry);
+    }
+}
