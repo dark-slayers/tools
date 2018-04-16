37097b3f4bf95c8b6e050c59ba85514390e8c6a7
dark-slayers <23534878@qq.com>
2018-04-16 17:30:32 +0800

    增加项目抽象类

---
+++ src/main/java/person/liuxx/tools/project/AbstractZipProject.java
@@ -0,0 +1,72 @@
+package person.liuxx.tools.project;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import person.liuxx.util.log.LogUtil;
+
+/**
+ * @author 刘湘湘
+ * @version 1.0.0<br>
+ *          创建时间：2018年4月16日 下午4:14:14
+ * @since 1.0.0
+ */
+public abstract class AbstractZipProject
+{
+    private Logger log = LoggerFactory.getLogger(AbstractZipProject.class);
+
+    public ByteArrayOutputStream createZipOutputStream() throws IOException
+    {
+        ByteArrayOutputStream o = new ByteArrayOutputStream();
+        ZipOutputStream zout = new ZipOutputStream(o);
+        Files.walk(path()).forEach(f ->
+        {
+            createZip(f, zout);
+        });
+        return o;
+    }
+
+    private void createZip(Path p, ZipOutputStream zout)
+    {
+        boolean isFile = !Files.isDirectory(p);
+        String fileName = path().relativize(p).toString() + (isFile ? "" : "/");
+        if (Objects.equals(fileName, "/"))
+        {
+            return;
+        }
+        try
+        {
+            ZipEntry ze = new ZipEntry(fileName);
+            zout.putNextEntry(ze);
+            if (isFile)
+            {
+                try (InputStream fis = createInputStream(p);)
+                {
+                    int j = 0;
+                    byte[] buffer = new byte[1024];
+                    while ((j = fis.read(buffer)) > 0)
+                    {
+                        zout.write(buffer, 0, j);
+                    }
+                }
+            }
+            zout.closeEntry();
+        } catch (IOException e)
+        {
+            log.error(LogUtil.errorInfo(e));
+        }
+    }
+
+    protected abstract InputStream createInputStream(Path p) throws IOException;
+
+    protected abstract Path path();
+}
--- src/main/java/person/liuxx/tools/project/ReactProject.java
+++ src/main/java/person/liuxx/tools/project/ReactProject.java
@@ -1,21 +1,13 @@
 package person.liuxx.tools.project;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import person.liuxx.util.base.StringUtil;
-import person.liuxx.util.log.LogUtil;
 
 /**
  * @author 刘湘湘
@@ -23,9 +15,8 @@ import person.liuxx.util.log.LogUtil;
  *          创建时间：2018年4月13日 下午2:05:48
  * @since 1.0.0
  */
-public class ReactProject
+public class ReactProject extends AbstractZipProject
 {
-    private Logger log = LoggerFactory.getLogger(ReactProject.class);
     private final String LF = new String(Character.toChars(0x0A));
     private final String projectName;
     private final Path path;
@@ -78,50 +69,8 @@ public class ReactProject
         return new ByteArrayInputStream(packageJson.getBytes("UTF-8"));
     }
 
-    public ByteArrayOutputStream createZipOutputStream() throws IOException
-    {
-        ByteArrayOutputStream o = new ByteArrayOutputStream();
-        ZipOutputStream zout = new ZipOutputStream(o);
-        Files.walk(path).forEach(f ->
-        {
-            createZip(f, zout);
-        });
-        return o;
-    }
-
-    private void createZip(Path p, ZipOutputStream zout)
-    {
-        boolean isFile = !Files.isDirectory(p);
-        String fileName = path.relativize(p).toString() + (isFile ? "" : "/");
-        if (Objects.equals(fileName, "/"))
-        {
-            return;
-        }
-        log.debug("fileName:{}", fileName);
-        try
-        {
-            ZipEntry ze = new ZipEntry(fileName);
-            zout.putNextEntry(ze);
-            if (isFile)
-            {
-                try (InputStream fis = createInputStream(p);)
-                {
-                    int j = 0;
-                    byte[] buffer = new byte[1024];
-                    while ((j = fis.read(buffer)) > 0)
-                    {
-                        zout.write(buffer, 0, j);
-                    }
-                }
-            }
-            zout.closeEntry();
-        } catch (IOException e)
-        {
-            log.error(LogUtil.errorInfo(e));
-        }
-    }
-
-    private InputStream createInputStream(Path p) throws IOException
+    @Override
+    protected InputStream createInputStream(Path p) throws IOException
     {
         switch (p.getFileName().toString())
         {
@@ -135,5 +84,11 @@ public class ReactProject
             }
         }
         return Files.newInputStream(p);
+    }
+
+    @Override
+    protected Path path()
+    {
+        return path;
     }
 }
